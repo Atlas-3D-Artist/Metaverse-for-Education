e9d9146031349bc84e5d46032716b14515a7689e
YoonSeok Oh <atlas.3d.artist@gmail.com>
2023-10-17 21:10:49 +0900

    Public release

---
+++ Assets/Metaverse-for-LMS/Scripts/SceneManage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+
+public class SceneManage : MonoBehaviour
+{
+    // #region  Playground
+
+    // public void Classroom()
+    // {
+    //     SceneManager.LoadScene("Classroom");
+    // }
+    
+    // public void Library()
+    // {
+    //     SceneManager.LoadScene("Library");
+    // }
+
+    // public void Studyroom()
+    // {
+    //     SceneManager.LoadScene("Studyroom");
+    // }
+
+    // #endregion
+}
